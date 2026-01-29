fcb8d6e6cfe1d1b6d304ef22fb631be467a0b535
rrmcmurry <rrmcmurry@gmail.com>
2026-01-29 15:16:46 -0500

    Add DriveSubsystem and SwerveModule

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,12 +4,20 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.DriveSubsystem;
 
 public class RobotContainer {
+
+  public final DriveSubsystem drive = new DriveSubsystem();
+  private final XboxController controller = new XboxController(0);
+
   public RobotContainer() {
     configureBindings();
+    CommandScheduler.getInstance().setDefaultCommand(drive, drive.driveCommand(controller, false));
   }
 
   private void configureBindings() {}
