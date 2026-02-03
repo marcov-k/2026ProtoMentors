d06997e9f0a09c10607de7ba0828bca9c0714b60
rrmcmurry <56413963+rrmcmurry@users.noreply.github.com>
2026-02-02 22:52:33 -0500

    Create Intake.java

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,40 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+@SuppressWarnings("removal")
+public class Intake extends SubsystemBase{
+
+    public static SparkMaxConfig DefaultConfig = new SparkMaxConfig();    
+    private SparkMax IntakeMotor;     
+    public static final int kIntakeMotorCanID = 9;
+
+    static {
+        DefaultConfig.smartCurrentLimit(50);
+        DefaultConfig.idleMode(IdleMode.kCoast);
+        DefaultConfig.openLoopRampRate(1.0);
+        DefaultConfig.inverted(true);
+    }
+
+    public Intake() {
+        IntakeMotor = new SparkMax(kIntakeMotorCanID, MotorType.kBrushless);
+        IntakeMotor.configure(DefaultConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+    }
+
+    public Command intake() {
+        return Commands.runOnce(() -> IntakeMotor.set(1));
+    }
+
+    public Command stop() {
+        return Commands.runOnce(() -> IntakeMotor.set(0));
+    }
+}
