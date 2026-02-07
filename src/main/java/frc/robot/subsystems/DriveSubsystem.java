826169c463c4065614c0828f530ded7f7b4d3b28
rrmcmurry <56413963+rrmcmurry@users.noreply.github.com>
2026-02-07 11:00:42 -0500

    Update DriveSubsystem.java
    Changed to CommandXBoxController and made kSpeedLimit a variable

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -6,7 +6,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -14,7 +14,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.studica.frc.AHRS;
 
 public class DriveSubsystem extends SubsystemBase {
-        public static final class DriveConstants {
+    
+    public static double kSpeedLimit = 0.2;
+    
+    public static final class DriveConstants {
 
         // SPARK MAX CAN IDs
         public static final int kFrontLeftDrivingCanId = 1;
@@ -48,82 +51,84 @@ public class DriveSubsystem extends SubsystemBase {
         public static final double kBackLeftChassisAngularOffset = Math.PI;
         public static final double kBackRightChassisAngularOffset = Math.PI / 2;
       
-    }  
-
-    // Create 4 instances of SwerveModules
-    private final SwerveModule m_frontLeft = new SwerveModule(
-        DriveConstants.kFrontLeftDrivingCanId,
-        DriveConstants.kFrontLeftTurningCanId,
-        DriveConstants.kFrontLeftChassisAngularOffset);
-
-    private final SwerveModule m_frontRight = new SwerveModule(
-        DriveConstants.kFrontRightDrivingCanId,
-        DriveConstants.kFrontRightTurningCanId,
-        DriveConstants.kFrontRightChassisAngularOffset);
-
-    private final SwerveModule m_rearLeft = new SwerveModule(
-        DriveConstants.kRearLeftDrivingCanId,
-        DriveConstants.kRearLeftTurningCanId,
-        DriveConstants.kBackLeftChassisAngularOffset);
-
-    private final SwerveModule m_rearRight = new SwerveModule(
-        DriveConstants.kRearRightDrivingCanId,
-        DriveConstants.kRearRightTurningCanId,
-        DriveConstants.kBackRightChassisAngularOffset);
-
-    // Create NavX AHRS Gyroscope
-    private final AHRS m_gyro = new AHRS(AHRS.NavXComType.kMXP_SPI);
-
-    /**
-     * Method to drive the robot using joystick info.
-     *
-     * @param forward       Speed of the robot in the forward, forward positive.
-     * @param strafe        Speed of the robot in the sideways direction, left negative.
-     * @param rotation      Rate of the robot rotation, left negative.
-     * @param fieldRelative Whether the provided speeds are relative to the field.   
-     */
-    public void drive(double forward, double strafe, double rotation, boolean fieldRelative) {
-
-        // Convert the commanded speeds into the correct units for the drivetrain, make strafe and rotation left positive numbers as expected for swerve
-        double forwardDelivered = forward * DriveConstants.kMaxSpeedMetersPerSecond;
-        double strafeDelivered = -strafe * DriveConstants.kMaxSpeedMetersPerSecond;
-        double rotDelivered = -rotation * DriveConstants.kMaxAngularSpeed;
-        double currentangle = -m_gyro.getAngle() % 360;
-
-        var swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
-            fieldRelative
-                ? ChassisSpeeds.fromFieldRelativeSpeeds(forwardDelivered, strafeDelivered, rotDelivered, Rotation2d.fromDegrees(currentangle))
-                : new ChassisSpeeds(forwardDelivered, strafeDelivered, rotDelivered));
-
-        SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, DriveConstants.kMaxSpeedMetersPerSecond);
-
-        m_frontLeft.setDesiredState(swerveModuleStates[0]);
-        m_frontRight.setDesiredState(swerveModuleStates[1]);
-        m_rearLeft.setDesiredState(swerveModuleStates[2]);
-        m_rearRight.setDesiredState(swerveModuleStates[3]);
-
     }
-
-    public void reset(){
-        m_gyro.zeroYaw();
-    }
-
-    public float getHeading() {// Assuming this returns yaw in degrees
-        return m_gyro.getYaw();
-    }
-
-    public void stop(){
-        this.drive(0,0,0,true);
-    }
-
-    public Command driveCommand(XboxController controller, boolean fieldRelative){
-        return Commands.run(
-            () -> {
-                double forward = MathUtil.applyDeadband(-controller.getLeftY() * 0.2, 0.020);
-                double strafe = MathUtil.applyDeadband(controller.getLeftX() * 0.2, 0.02);
-                double rotate = MathUtil.applyDeadband(controller.getRightX() * 0.2, 0.02);
-                this.drive(forward, strafe, rotate, fieldRelative);
-            }
+    
+        
+    
+        // Create 4 instances of SwerveModules
+        private final SwerveModule m_frontLeft = new SwerveModule(
+            DriveConstants.kFrontLeftDrivingCanId,
+            DriveConstants.kFrontLeftTurningCanId,
+            DriveConstants.kFrontLeftChassisAngularOffset);
+    
+        private final SwerveModule m_frontRight = new SwerveModule(
+            DriveConstants.kFrontRightDrivingCanId,
+            DriveConstants.kFrontRightTurningCanId,
+            DriveConstants.kFrontRightChassisAngularOffset);
+    
+        private final SwerveModule m_rearLeft = new SwerveModule(
+            DriveConstants.kRearLeftDrivingCanId,
+            DriveConstants.kRearLeftTurningCanId,
+            DriveConstants.kBackLeftChassisAngularOffset);
+    
+        private final SwerveModule m_rearRight = new SwerveModule(
+            DriveConstants.kRearRightDrivingCanId,
+            DriveConstants.kRearRightTurningCanId,
+            DriveConstants.kBackRightChassisAngularOffset);
+    
+        // Create NavX AHRS Gyroscope
+        private final AHRS m_gyro = new AHRS(AHRS.NavXComType.kMXP_SPI);
+    
+        /**
+         * Method to drive the robot using joystick info.
+         *
+         * @param forward       Speed of the robot in the forward, forward positive.
+         * @param strafe        Speed of the robot in the sideways direction, left negative.
+         * @param rotation      Rate of the robot rotation, left negative.
+         * @param fieldRelative Whether the provided speeds are relative to the field.   
+         */
+        public void drive(double forward, double strafe, double rotation, boolean fieldRelative) {
+    
+            // Convert the commanded speeds into the correct units for the drivetrain, make strafe and rotation left positive numbers as expected for swerve
+            double forwardDelivered = forward * DriveConstants.kMaxSpeedMetersPerSecond;
+            double strafeDelivered = -strafe * DriveConstants.kMaxSpeedMetersPerSecond;
+            double rotDelivered = -rotation * DriveConstants.kMaxAngularSpeed;
+            double currentangle = -m_gyro.getAngle() % 360;
+    
+            var swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
+                fieldRelative
+                    ? ChassisSpeeds.fromFieldRelativeSpeeds(forwardDelivered, strafeDelivered, rotDelivered, Rotation2d.fromDegrees(currentangle))
+                    : new ChassisSpeeds(forwardDelivered, strafeDelivered, rotDelivered));
+    
+            SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, DriveConstants.kMaxSpeedMetersPerSecond);
+    
+            m_frontLeft.setDesiredState(swerveModuleStates[0]);
+            m_frontRight.setDesiredState(swerveModuleStates[1]);
+            m_rearLeft.setDesiredState(swerveModuleStates[2]);
+            m_rearRight.setDesiredState(swerveModuleStates[3]);
+    
+        }
+    
+        public void reset(){
+            m_gyro.zeroYaw();
+        }
+    
+        public float getHeading() {// Assuming this returns yaw in degrees
+            return m_gyro.getYaw();
+        }
+    
+        public void stop(){
+            this.drive(0,0,0,true);
+        }
+    
+        public Command driveCommand(CommandXboxController controller, boolean fieldRelative){
+            return Commands.run(
+                () -> {
+                    double forward = MathUtil.applyDeadband(-controller.getLeftY() * kSpeedLimit, 0.020);
+                    double strafe = MathUtil.applyDeadband(controller.getLeftX() * kSpeedLimit, 0.02);
+                    double rotate = MathUtil.applyDeadband(controller.getRightX() * kSpeedLimit, 0.02);
+                    this.drive(forward, strafe, rotate, fieldRelative);
+                }
             , this);
     } 
 }
