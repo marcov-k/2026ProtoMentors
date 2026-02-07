ec94227dc7aa950433e2038d863cf694cfb6185e
rrmcmurry <56413963+rrmcmurry@users.noreply.github.com>
2026-02-07 11:01:18 -0500

    Update RobotContainer.java
    Added bindings for XBoxController to run the intake and the launcher.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,23 +4,28 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.*;
 
 public class RobotContainer {
 
   public final DriveSubsystem drive = new DriveSubsystem();
-  private final XboxController controller = new XboxController(0);
+  public final Intake intake = new Intake();
+  public final Launcher launcher = new Launcher();
+  private final CommandXboxController controller = new CommandXboxController(0);
 
   public RobotContainer() {
     configureBindings();
     CommandScheduler.getInstance().setDefaultCommand(drive, drive.driveCommand(controller, false));
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    controller.leftTrigger().onTrue(intake.run()).onFalse(intake.stop());
+    controller.rightTrigger().onTrue(launcher.run()).onFalse(launcher.stop());
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
