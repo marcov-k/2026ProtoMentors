05d2da9b690719ca554ecc91d62544124a6c2abf
Marco Vasko-Klima <marcoaklima@gmail.com>
2026-02-28 13:30:23 -0500

    Cleaned up more code

--- src/main/java/frc/robot/field/FieldConstants.java
+++ src/main/java/frc/robot/field/FieldConstants.java
@@ -17,11 +17,11 @@ public final class FieldConstants {
     public static final Rectangle2d RED_ALLIANCE_ZONE = new Rectangle2d(new Translation2d(11.94,0), new Translation2d(16.5, 8.0));
 
     public static class StartingPositions {
-        public static final Pose2d BLUE_STATION_1 = new Pose2d(new Translation2d(4.0,2.3),Rotation2d.fromDegrees(0));
-        public static final Pose2d BLUE_STATION_2 = new Pose2d(new Translation2d(4.0,4.0),Rotation2d.fromDegrees(0));
-        public static final Pose2d BLUE_STATION_3 = new Pose2d(new Translation2d(4.0,5.7),Rotation2d.fromDegrees(0));
-        public static final Pose2d RED_STATION_1 = new Pose2d(new Translation2d(12.56,2.3),Rotation2d.fromDegrees(180));
-        public static final Pose2d RED_STATION_2 = new Pose2d(new Translation2d(12.56,4.0),Rotation2d.fromDegrees(180));
-        public static final Pose2d RED_STATION_3 = new Pose2d(new Translation2d(12.56,5.7),Rotation2d.fromDegrees(180));
+        public static final Pose2d BLUE_STATION_1 = new Pose2d(new Translation2d(4.0,2.3), Rotation2d.fromDegrees(0));
+        public static final Pose2d BLUE_STATION_2 = new Pose2d(new Translation2d(4.0,4.0), Rotation2d.fromDegrees(0));
+        public static final Pose2d BLUE_STATION_3 = new Pose2d(new Translation2d(4.0,5.7), Rotation2d.fromDegrees(0));
+        public static final Pose2d RED_STATION_1 = new Pose2d(new Translation2d(12.56,2.3), Rotation2d.fromDegrees(180));
+        public static final Pose2d RED_STATION_2 = new Pose2d(new Translation2d(12.56,4.0), Rotation2d.fromDegrees(180));
+        public static final Pose2d RED_STATION_3 = new Pose2d(new Translation2d(12.56,5.7), Rotation2d.fromDegrees(180));
     }
 }
