1105604f4c88d0783aca887cd0ce24e7ec755514
rrmcmurry <56413963+rrmcmurry@users.noreply.github.com>
2026-03-08 00:52:18 -0500

    Update AllianceUtil.java
    Moved Alliance Related Pose getters to Alliance Utility class.

--- src/main/java/frc/robot/field/AllianceUtil.java
+++ src/main/java/frc/robot/field/AllianceUtil.java
@@ -2,6 +2,7 @@ package frc.robot.field;
 
 import java.util.Optional;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 
@@ -17,5 +18,65 @@ public class AllianceUtil {
                 ? FieldConstants.RED_HUB_CENTER
                 : FieldConstants.BLUE_HUB_CENTER;
     }
+
+    // Returns an estimated default Starting Position based on Alliance and Station number
+    public static Pose2d getStartingPose() {
+        Optional<DriverStation.Alliance> allianceOpt = DriverStation.getAlliance();
+        int station = DriverStation.getLocation().orElse(2);
+        boolean isRed = allianceOpt.isPresent() && allianceOpt.get() == DriverStation.Alliance.Red;
+        Pose2d startingPose = new Pose2d();        
+
+        if (!isRed) {
+            // BLUE side
+            switch (station) {
+                case 1: startingPose = FieldConstants.StartingPositions.BLUE_STATION_1; break;
+                case 2: startingPose = FieldConstants.StartingPositions.BLUE_STATION_2; break;
+                case 3: startingPose = FieldConstants.StartingPositions.BLUE_STATION_3; break;
+                default: startingPose = FieldConstants.StartingPositions.BLUE_STATION_1;
+            }
+
+        } else {
+            // RED side
+            switch (station) {
+                case 1: startingPose = FieldConstants.StartingPositions.RED_STATION_1; break;
+                case 2: startingPose = FieldConstants.StartingPositions.RED_STATION_2; break;
+                case 3: startingPose = FieldConstants.StartingPositions.RED_STATION_3; break;
+                default: startingPose = FieldConstants.StartingPositions.RED_STATION_1;
+            }
+        }
+        
+        return startingPose;
+    }
+
+
+    // Returns an estimated default Starting Position based on Alliance and Station number
+    public static Translation2d getAutonomousFiringPosition() {
+        Optional<DriverStation.Alliance> allianceOpt = DriverStation.getAlliance();
+        int station = DriverStation.getLocation().orElse(2);
+        boolean isRed = allianceOpt.isPresent() && allianceOpt.get() == DriverStation.Alliance.Red;
+        Translation2d firingPose = new Translation2d();        
+
+        if (!isRed) {
+            // BLUE side
+            switch (station) {
+                case 1: firingPose = FieldConstants.AutoFiringPositions.BLUE_STATION_1; break;
+                case 2: firingPose = FieldConstants.AutoFiringPositions.BLUE_STATION_2; break;
+                case 3: firingPose = FieldConstants.AutoFiringPositions.BLUE_STATION_3; break;
+                default: firingPose = FieldConstants.AutoFiringPositions.BLUE_STATION_1;
+            }
+
+        } else {
+            // RED side
+            switch (station) {
+                case 1: firingPose = FieldConstants.AutoFiringPositions.RED_STATION_1; break;
+                case 2: firingPose = FieldConstants.AutoFiringPositions.RED_STATION_2; break;
+                case 3: firingPose = FieldConstants.AutoFiringPositions.RED_STATION_3; break;
+                default: firingPose = FieldConstants.AutoFiringPositions.RED_STATION_1;
+            }
+        }
+        
+        return firingPose;
+    }
+
 }
 
